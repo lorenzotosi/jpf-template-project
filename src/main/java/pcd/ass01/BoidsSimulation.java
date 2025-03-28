c9daaa8645eea4a774dd85cf3173b883a8ff855c
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-03-28 09:37:57 +0100

    change nboids

--- src/main/java/pcd/ass01/BoidsSimulation.java
+++ src/main/java/pcd/ass01/BoidsSimulation.java
@@ -4,7 +4,7 @@ import pcd.ass01.monitor.SimulationMonitor;
 
 public class BoidsSimulation {
 
-	final static int N_BOIDS = 1500;
+	final static int N_BOIDS = 50;
 
 	final static double SEPARATION_WEIGHT = 1.0;
     final static double ALIGNMENT_WEIGHT = 1.0;
